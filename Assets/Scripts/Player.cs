3b8822467541ad2967e5c1c076d00baf42d3108a
rlaghkdgus <rlaghkdgus@gmail.com>
2024-09-30 05:14:20 +0900

    09/30 더미

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,30 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class Player : Singleton<Player>
 {
-    [SerializeField] ItemData items;
+    [SerializeField] ItemData dItems;
     public List<Item> playerItems;
     public int money;
+    public Image pImage;
+    public TMP_Text priceText;
     
-    private void Buy(Item item)
+    /*
+     * 현재 코드의 문제점 : 어떤 상황에서 UI및 아이템을 세팅할것인가
+     * 현재 작동 방식 : item을 셋팅 -> money가 부족하면 바로 리턴 -> UI셋팅 -> 돈 제거 후 리스트삽입
+     */
+    public void SetUI(Item item)
     {
+        pImage.sprite = item.image;
+        priceText.text = "" + item.price;
+    }
+    public void Buy()
+    {
+        Item item = dItems.items[Random.Range(0, dItems.items.Length)];
         if(money < item.price)
         {
             //불가능 UI
             return;
         }
+        SetUI(item);
         money -= item.price;
         for(int i = 0; i < playerItems.Count; i++)
         {
-            if (playerItems[i] == item)
+            if (playerItems[i].stuffName == item.stuffName)
+            {
                 playerItems[i].counts += 1;
-            else
-                playerItems.Add(item);
+                return;
+            }
         }
+        playerItems.Add(item);
+        return;
     }
-    private void Sell(Item item)
+    public void Sell()
     {
        
     }
